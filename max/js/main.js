719f58b1fbaef32c9c2218e29ac5def9b705fa89
Liza <elizaveta.selivanova.eu@gmail.com>
2015-03-28 20:50:41 +0300

    qwerty

--- max/js/main.js
+++ max/js/main.js
@@ -146,7 +146,7 @@ function startEdit() {
             target.style.transform =
                 'translate(' + x + 'px, ' + y + 'px)';
 
-        // update the posiion attributes
+        // update the position attributes
         target.setAttribute('data-x', x);
         target.setAttribute('data-y', y);
     };
