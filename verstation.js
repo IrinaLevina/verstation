469f7b426a728a08127461991976a7f937b97802
Irina Levina <irina.levina.i@gmail.com>
2015-03-28 18:12:10 +0300

    fefef

--- verstation.js
+++ verstation.js
@@ -1,12 +1,16 @@
 var datamass= null,
 customDataMass = null;
+var globalCurrentDirection = 'left',
+	globaCurrentPoint,
+	nextPoint = {},
+	resultStack = new Array();
 $(document).ready(function(){
 	// создаем или находим изображение
 	var img = $('.markup'),
 	width = $('.markup').width(),
 	height = $('.markup').height();
 	var markup = document.getElementById("markup");
-	img.load( function() {
+	$('.button-generate').click( function() {
 		// создаем или находим canvas
 		var canvas = document.getElementById('canvas');
 		// получаем его 2D контекст
@@ -53,10 +57,10 @@ var analizePixels = function(canvasPixels){
 				var colorSumm = changeToHSL(canvasPixels[i-3], canvasPixels[i-2], canvasPixels[i-1]);
 				if (colorSumm < 300 && canvasPixels[i]!=0){
 					pixMas[indexRow][indexCol] = 1
-					console.log(colorSumm);
+					/*console.log(colorSumm);
 					console.log("index = " + indexRow+ " "+ indexCol )
 					console.log("params = " + canvasPixels[i-3] +" "+ canvasPixels[i-2]+" "+canvasPixels[i-1]+" "+canvasPixels[i])
-				}else{
+				*/}else{
 					pixMas[indexRow][indexCol] = 0
 				}
 
@@ -72,6 +76,7 @@ var analizePixels = function(canvasPixels){
 		}
 	customDataMass = pixMas;
 	outPut(pixMas);
+	algorithmFindContur();
 }
 
 	var changeToHSL = function(r,g,b){
@@ -94,6 +99,100 @@ var analizePixels = function(canvasPixels){
 		$('.ouput-mass').append(htmlTemplate);
 	};
 
+
+
 	var algorithmFindContur = function(){
-		
+		var direction = 'right',
+			currentColor = 0;
+		var i = 0; 
+		var j = 0;
+		var currentPoint = {};
+		var current = false;
+		currentPoint["i"] = 0;
+		currentPoint["j"] = 0;
+		for (i = 0; i < customDataMass.length; i++){
+			if (!current){
+				for (j = 0; j < customDataMass[i].length; j++){
+					if (!current){
+						if (customDataMass[i][j] != currentColor){
+							current = true;
+							currentPoint["i"] = i;
+							currentPoint["j"] = j;
+						}
+					}
+				}
+			}
+		}
+		globaCurrentPoint = currentPoint;
+		if (current){
+			goAroundContur(globaCurrentPoint); 
+		}
 	}
+var goAroundContur = function(currentPoint){
+	var stack = [],
+		direction = 'right';
+	var firstPoint = globaCurrentPoint;
+	var nextElement = null;
+	var directionTo;
+	var moving = false;
+	move(globaCurrentPoint, direction);
+	while (globaCurrentPoint["i"] != firstPoint["i"] || globaCurrentPoint["j"] != firstPoint["j"]){
+		moving = true;
+		move(globaCurrentPoint, direction, color);
+		if (customDataMass[globaCurrentPoint["i"]][globaCurrentPoint["j"]] == 1){
+			resultStack.push(globaCurrentPoint);
+		}
+	}
+	
+	console.log(resultStack)
+}
+
+var move = function(point, direction){
+	if (customDataMass[point["i"]][point["j"]] == 1){
+		switch(direction) {
+		    case "left":
+		        globalCurrentDirection = "top";
+		        globaCurrentPoint["i"] = point["i"]-1;
+		        globaCurrentPoint["j"] = point["j"];
+		        break;
+		    case "right":
+		        globalCurrentDirection = "bottom";
+		        globaCurrentPoint["i"] = point["i"]+1;
+		        globaCurrentPoint["j"] = point["j"];
+		        break; 
+		    case "top":
+		        globalCurrentDirection = "right";
+		        globaCurrentPoint["i"] = point["i"];
+		        globaCurrentPoint["j"] = point["j"]+1;
+		        break;
+	        case "botton":
+		        globalCurrentDirection = "left";
+		        globaCurrentPoint["i"] = point["i"];
+		        globaCurrentPoint["j"] = point["j"]-1;
+		        break;
+		}
+	} else if (customDataMass[point["i"]][point["j"]] == 0) {
+		switch(direction) {
+		    case "left":
+		        globalCurrentDirection = "bottom";
+		        nextPoint["i"] = point["i"]+1;
+		        nextPoint["j"] = point["j"];
+		        break;
+		    case "right":
+		        globalCurrentDirection = "top";
+		        nextPoint["i"] = point["i"]-1;
+		        nextPoint["j"] = point["j"];
+		        break; 
+		    case "top":
+		        globalCurrentDirection = "left";
+		        nextPoint["i"] = point["i"];
+		        nextPoint["j"] = point["j"]-1;
+		        break;
+	        case "botton":
+		        globalCurrentDirection = "right";
+		        nextPoint["i"] = point["i"];
+		        nextPoint["j"] = point["j"]+1;
+		        break;
+		}
+	}
+}
